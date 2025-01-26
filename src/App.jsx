4329d07e655597c0a11d78f5f21e3cc28f5bc9dd
alexandresc <alexandrenescau@gmail.com>
2025-01-26 18:30:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Navigation from './Navigation'
+import Hero1 from './Hero1'
+import Timeline from './Timeline'
+import Stats from './Stats'
+import Card from './Card'
+import Footer from './Footer'
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+      <Hero1 />
+      <Timeline />
+      <Stats />
+      <Card />
+      <Footer />
+    </>
+  )
+}
+
+export default App
