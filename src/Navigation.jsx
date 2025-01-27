4ba9244fa95e4376a9d0f6324cb585062a5c1774
alexandresc <alexandrenescau@gmail.com>
2025-01-26 23:00:25 -0300

    fix: make navbar sticky at the top of the viewport

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navigation() {
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100 shadow-sm sticky top-0 z-100">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
